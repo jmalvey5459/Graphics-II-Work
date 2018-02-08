6ba08cbfdeb0205b07fbb095937043929e1b8565
Jack <jmalvey5459@gmail.com>
2018-02-08 02:05:54 -0500

    fragment shader done, works adding files to rep that i forgot to add before

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/03-framebuffer/drawAttribs_mrt_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/03-framebuffer/drawAttribs_mrt_fs4x.glsl
@@ -25,14 +25,44 @@
 #version 410
 
 // ****TO-DO: 
-//	1) declare varying block to receive attribute data
-//	2) declare MRT
-//	3) copy inbound data to outbound render targets (might need to compress)
+//	1) declare varying block to receive attribute data [x]
+//	2) declare MRT[x]
+//	3) copy inbound data to outbound render targets (might need to compress)[x]
 
-out vec4 rtFragColor;
+
+//goes to each of the demo modes in order
+layout (location = 0	) out vec4	rtPosition;
+layout (location = 1	) out vec4	rtNormal;
+layout (location = 2	) out vec4	rtColor;
+layout (location = 3	) out vec4	rtTexcoord;
+layout (location = 4	) out vec4	rtTagent;
+layout (location = 5	) out vec4	rtBitangent;
+layout (location = 6	) out vec4	rtBlendWeight;
+layout (location = 7	) out vec4	rtBlendIndices;
+
+in vbPassDataBlock
+{
+	vec4 vPosition;
+	vec3 vNormal;
+	vec3 vColor;
+	vec2 vTexcoord;
+	vec3 vTagent;
+	vec3 vBitangent;
+	vec3 vBlendWeight;
+	vec3 vBlendIndices;
+} vPassData;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE WHITE
-	rtFragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+
+	//thomas told me about the *0.5 + 0.5 clamp being needed for this
+	rtPosition	   = vec4(normalize(vPassData.vPosition.rgb *0.5)+0.5,1.0);
+	rtNormal	   = vec4(normalize(vPassData.vNormal*0.5)+0.5,1.0);
+	rtColor		   = vec4(vPassData.vColor,1.0);
+	rtTexcoord	   = vec4(vPassData.vTexcoord,0.0,1.0);
+	rtTagent	   = vec4(normalize(vPassData.vTagent*0.5)+0.5,1.0);
+	rtBitangent    = vec4(normalize(vPassData.vBitangent*0.5)+0.5,1.0);
+	rtBlendWeight  = vec4(vPassData.vBlendWeight,1.0);
+	rtBlendIndices = vec4(normalize(vPassData.vBlendIndices*0.5)+0.5,1.0);
 }
